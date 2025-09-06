6a6bfb9a5a6c427dcba1d7c3777be3af5be44b70
dauntless <3420130533@qq.com>
2025-09-06 20:06:35 +0800

    提交至MQ然后异步调AI

--- src/main/java/com/yupi/springbootinit/bizmq/BiMessageConsumer.java
+++ src/main/java/com/yupi/springbootinit/bizmq/BiMessageConsumer.java
@@ -1,5 +1,7 @@
 package com.yupi.springbootinit.bizmq;
 
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.rabbitmq.client.Channel;
 import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.constant.CommonConstant;
@@ -16,6 +18,8 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Component
 @Slf4j
@@ -26,6 +30,9 @@ public class BiMessageConsumer {
 
     @Resource
     private AiManager aiManager;
+
+    @Resource
+    private ThreadPoolExecutor threadPoolExecutor;
 
     // 指定程序监听的消息队列和确认机制
     @SneakyThrows
@@ -38,43 +45,83 @@ public class BiMessageConsumer {
             channel.basicNack(deliveryTag, false, false);
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "消息为空");
         }
-        long chartId = Long.parseLong(message);
-        Chart chart = chartService.getById(chartId);
+//        JSONObject jsonObject= new JSONObject();
+        //JSONObject jsonObject = JSONUtil.parseObj(message);
+        //Long chartId = jsonObject.getLong("chartId");
+        Chart chart = chartService.getById(message);
+//        Chart chart = chartService.getById(chartId);
         if (chart == null) {
             channel.basicNack(deliveryTag, false, false);
             throw new BusinessException(ErrorCode.NOT_FOUND_ERROR, "图表为空");
         }
+
+        //异步调AI开始，是优化还是冗余设计？
+        // todo 建议处理任务队列满了后，抛异常的情况
+        CompletableFuture.runAsync(() -> {
+// 先修改图表任务状态为 “执行中”。等执行成功后，修改为 “已完成”、保存执行结果；执行失败后，状态修改为 “失败”，记录任务失败信息。
+            Chart updateChart = new Chart();
+            updateChart.setId(chart.getId());
+            updateChart.setStatus("running");
+            boolean b = chartService.updateById(updateChart);
+            if (!b) {
+                handleChartUpdateError(chart.getId(), "更新图表执行中状态失败");
+                return;
+            }
+// 调用 AI
+            String result = aiManager.sendMsgToXingHuo (true, buildUserInput(chart));
+            String[] splits = result.split("'【【【【'");
+            if (splits.length < 3) {
+                handleChartUpdateError(chart.getId(), "AI 生成错误");
+                return;
+            }
+            String genChart = splits[1].trim();
+            String genResult = splits[2].trim();
+            Chart updateChartResult = new Chart();
+            updateChartResult.setId(chart.getId());
+            updateChartResult.setGenChart(genChart);
+            updateChartResult.setGenResult(genResult);
+// todo 建议定义状态为枚举值
+            updateChartResult.setStatus("succeed");
+            boolean updateResult = chartService.updateById(updateChartResult);
+            if (!updateResult) {
+                handleChartUpdateError(chart.getId(), "更新图表成功状态失败");
+            }
+        }, threadPoolExecutor);
+        //异步结束
+
+
+
         // 先修改图表任务状态为 “执行中”。等执行成功后，修改为 “已完成”、保存执行结果；执行失败后，状态修改为 “失败”，记录任务失败信息。
-        Chart updateChart = new Chart();
-        updateChart.setId(chart.getId());
-        updateChart.setStatus("running");
-        boolean b = chartService.updateById(updateChart);
-        if (!b) {
-            channel.basicNack(deliveryTag, false, false);
-            handleChartUpdateError(chart.getId(), "更新图表执行中状态失败");
-            return;
-        }
-        // 调用 AI
-        String result = aiManager.sendMsgToXingHuo (true, buildUserInput(chart));
-        String[] splits = result.split("'【【【【'");
-        if (splits.length < 3) {
-            channel.basicNack(deliveryTag, false, false);
-            handleChartUpdateError(chart.getId(), "AI 生成错误");
-            return;
-        }
-        String genChart = splits[1].trim();
-        String genResult = splits[2].trim();
-        Chart updateChartResult = new Chart();
-        updateChartResult.setId(chart.getId());
-        updateChartResult.setGenChart(genChart);
-        updateChartResult.setGenResult(genResult);
-        // todo 建议定义状态为枚举值
-        updateChartResult.setStatus("succeed");
-        boolean updateResult = chartService.updateById(updateChartResult);
-        if (!updateResult) {
-            channel.basicNack(deliveryTag, false, false);
-            handleChartUpdateError(chart.getId(), "更新图表成功状态失败");
-        }
+//        Chart updateChart = new Chart();
+//        updateChart.setId(chart.getId());
+//        updateChart.setStatus("running");
+//        boolean b = chartService.updateById(updateChart);
+//        if (!b) {
+//            channel.basicNack(deliveryTag, false, false);
+//            handleChartUpdateError(chart.getId(), "更新图表执行中状态失败");
+//            return;
+//        }
+//        // 调用 AI
+//        String result = aiManager.sendMsgToXingHuo (true, buildUserInput(chart));
+//        String[] splits = result.split("'【【【【'");
+//        if (splits.length < 3) {
+//            channel.basicNack(deliveryTag, false, false);
+//            handleChartUpdateError(chart.getId(), "AI 生成错误");
+//            return;
+//        }
+//        String genChart = splits[1].trim();
+//        String genResult = splits[2].trim();
+//        Chart updateChartResult = new Chart();
+//        updateChartResult.setId(chart.getId());
+//        updateChartResult.setGenChart(genChart);
+//        updateChartResult.setGenResult(genResult);
+//        // todo 建议定义状态为枚举值
+//        updateChartResult.setStatus("succeed");
+//        boolean updateResult = chartService.updateById(updateChartResult);
+//        if (!updateResult) {
+//            channel.basicNack(deliveryTag, false, false);
+//            handleChartUpdateError(chart.getId(), "更新图表成功状态失败");
+//        }
         // 消息确认
         channel.basicAck(deliveryTag, false);
     }
